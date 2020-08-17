e7c1ec9ffce642eb6d65d1c9244b0ba5c068442d
Juan Agudelo <calijuma@gmail.com>
2020-08-17 18:18:38 -0400

    End of section 5

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,13 +1,13 @@
+using API.Extensions;
 using API.Helpers;
+using API.Middleware;
 using AutoMapper;
-using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 
 namespace API
 {
@@ -24,20 +24,27 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddScoped<IProductRepository, ProductRepository>();
-            services.AddScoped(typeof(IGenericRepository<>), (typeof(GenericRepository<>))); // added to work with generics
+            
             services.AddAutoMapper(typeof(MappingProfiles));
             services.AddControllers();
             services.AddDbContext<StoreContext>(x => x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
+            services.AddApplicationServices(); // Added from Extension/ApplicationServicesExtensions
+            services.AddSwaggerDocumentation(); // Added from Extension/SwaggerServiceExtensions
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            // if (env.IsDevelopment())
+            // {
+            //     app.UseDeveloperExceptionPage();
+            // }
+
+            // Replace it with our own exception handling
+            app.UseMiddleware<ExceptionMiddleware>();
+
+            app.UseStatusCodePagesWithReExecute("/errors/{0}"); // Error Middleware
 
             app.UseHttpsRedirection();
 
@@ -46,6 +53,9 @@ namespace API
             app.UseStaticFiles(); // To serve static content like images  - Order important
 
             app.UseAuthorization();
+
+            app.UseSwaggerDocumention(); // Added from Extension/SwaggerServiceExtensions
+
 
             app.UseEndpoints(endpoints =>
             {
