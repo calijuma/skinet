40e721a12f45d861847b577b385e81293b8ced4f
Juan Agudelo <calijuma@gmail.com>
2020-09-11 16:23:06 -0400

    End of section 20

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -17,6 +17,12 @@ const routes: Routes = [
     .then(mod => mod.BasketModule), data: {breadcrumb: 'Basket'}},
   {path: 'checkout', canActivate: [AuthGuard], loadChildren: () => import('./checkout/checkout.module')
     .then(mod => mod.CheckoutModule), data: {breadcrumb: 'Checkout'}},
+  {
+    path: 'orders',
+    canActivate: [AuthGuard],
+    loadChildren: () => import('./orders/orders.module')
+      .then(mod => mod.OrdersModule), data: { breadcrumb: 'Orders' }
+  },
   {path: 'account', loadChildren: () => import('./account/account.module')
     .then(mod => mod.AccountModule), data: {breadcrumb: {skip: true}}},
   {path: '**', redirectTo: 'not-found', pathMatch: 'full'}
